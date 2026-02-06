7b565551ecfec9e4e975753bc2b9af2d10634a30
shainmondal0192-del <shainmondal0192@gmail.com>
2026-02-06 23:03:46 +0530

    Disable NO button mouseover movement
    Comment out the mouseover event listener for the NO button.

--- script.js
+++ script.js
@@ -22,19 +22,19 @@ envelope.addEventListener("click", () => {
 
 // Logic to move the NO btn
 
-noBtn.addEventListener("mouseover", () => {
-    const min = 200;
-    const max = 200;
+// noBtn.addEventListener("mouseover", () => {
+//     const min = 200;
+//     const max = 200;
 
-    const distance = Math.random() * (max - min) + min;
-    const angle = Math.random() * Math.PI * 2;
+//     const distance = Math.random() * (max - min) + min;
+//     const angle = Math.random() * Math.PI * 2;
 
-    const moveX = Math.cos(angle) * distance;
-    const moveY = Math.sin(angle) * distance;
+//     const moveX = Math.cos(angle) * distance;
+//     const moveY = Math.sin(angle) * distance;
 
-    noBtn.style.transition = "transform 0.3s ease";
-    noBtn.style.transform = `translate(${moveX}px, ${moveY}px)`;
-});
+//     noBtn.style.transition = "transform 0.3s ease";
+//     noBtn.style.transform = `translate(${moveX}px, ${moveY}px)`;
+// });
 
 // Logic to make YES btn to grow
 
